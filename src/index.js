f1db16e040c1d64836ccc3ba3577875b89301c55
Pavel Svitek <svitek.pavel@gmail.com>
2017-08-27 15:04:06 +0200

    created containers/ and moved App

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Store from './store';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = Store();
